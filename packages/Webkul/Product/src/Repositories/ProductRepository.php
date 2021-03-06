0b01cce5a4d1c99939eb634aa165c4df4a36b898
Md Rezaul Karim <rkreza24@gmail.com>
2021-03-07 05:53:05 +0600

    Update ProductRepository.php

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -405,6 +405,7 @@ class ProductRepository extends Repository
                 ->orderBy('product_id', 'desc')
                 ->paginate(16);
         } else {
+            dd($term);
             $results = app(ProductFlatRepository::class)->scopeQuery(function ($query) use ($term, $channel, $locale) {
                 return $query->distinct()
                     ->addSelect('product_flat.*')
@@ -413,19 +414,14 @@ class ProductRepository extends Repository
                     ->where('product_flat.channel', $channel)
                     ->where('product_flat.locale', $locale)
                     ->whereNotNull('product_flat.url_key')
-                    ->orWhere('product_flat.name', 'like', '%' . urldecode($term) . '%')
-                    ->orWhere('product_flat.short_description', 'like', '%' . urldecode($term) . '%')
-                    ->orWhere('product_flat.meta_keywords', 'like', '%' . urldecode($term) . '%')
+                    ->where(function ($subQuery) use ($term) {
+                        $queries = explode('_', $term);
 
-
-                    // ->where(function ($subQuery) use ($term) {
-                    //     $queries = explode('_', $term);
-
-                    //     foreach (array_map('trim', $queries) as $value) {
-                    //         $subQuery->orWhere('product_flat.name', 'like', '%' . urldecode($value) . '%')
-                    //             ->orWhere('product_flat.short_description', 'like', '%' . urldecode($value) . '%');
-                    //     }
-                    // })
+                        foreach (array_map('trim', $queries) as $value) {
+                            $subQuery->orWhere('product_flat.name', 'like', '%' . urldecode($value) . '%')
+                                ->orWhere('product_flat.short_description', 'like', '%' . urldecode($value) . '%');
+                        }
+                    })
                     ->orderBy('product_id', 'desc');
             })->paginate(16);
         }
