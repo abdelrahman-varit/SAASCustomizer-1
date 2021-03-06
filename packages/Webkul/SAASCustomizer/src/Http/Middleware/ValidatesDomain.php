372b6fe6b83fd6bcc3b908759a5a3a411c767319
Md Rezaul Karim <rkreza24@gmail.com>
2021-03-07 05:08:36 +0600

    Update ValidatesDomain.php

--- packages/Webkul/SAASCustomizer/src/Http/Middleware/ValidatesDomain.php
+++ packages/Webkul/SAASCustomizer/src/Http/Middleware/ValidatesDomain.php
@@ -112,7 +112,7 @@ class ValidatesDomain
                             } else {
                                 $path = 'saas';
 
-                                return $this->response($path, 400, 'Store is deactivated!');
+                                return $this->response($path, 400, trans('saas::app.admin.tenant.exceptions.domain-not-found'), 'domain_not_found');
                                 // throw new \Exception('domain_not_found', 400);
                             }
                         }
@@ -121,9 +121,9 @@ class ValidatesDomain
                     }
 
                 }else{
-                    $path = 'saas';
+                    $path = 'sellnoir.com';
 
-                    return $this->response($path, 400, trans('saas::app.admin.tenant.exceptions.domain-not-found'), 'domain_not_found');
+                    return $this->response($path, 400, 'Store is deactivated!', 'Store Deactivated!');
                 }
 
 
