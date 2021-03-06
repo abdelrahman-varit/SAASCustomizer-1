e57bbd937559c3975963585c7d544d0ca4dfc220
Md Rezaul Karim <rkreza24@gmail.com>
2021-03-07 05:02:47 +0600

    Update ValidatesDomain.php

--- packages/Webkul/SAASCustomizer/src/Http/Middleware/ValidatesDomain.php
+++ packages/Webkul/SAASCustomizer/src/Http/Middleware/ValidatesDomain.php
@@ -120,6 +120,10 @@ class ValidatesDomain
                         return $next($request);
                     }
 
+                }else{
+                    $path = 'saas';
+
+                    return $this->response($path, 400, trans('saas::app.admin.tenant.exceptions.domain-not-found'), 'domain_not_found');
                 }
 
 
