615283731a3d25134af90ad2edbeb7802d33fffb
Md Rezaul Karim <rkreza24@gmail.com>
2021-03-07 05:49:34 +0600

    Update ProductRepository.php

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -413,19 +413,14 @@ class ProductRepository extends Repository
                     ->where('product_flat.channel', $channel)
                     ->where('product_flat.locale', $locale)
                     ->whereNotNull('product_flat.url_key')
-                    ->where('product_flat.name', 'like', '%' . urldecode($term) . '%')
-                    ->orWhere('product_flat.short_description', 'like', '%' . urldecode($term) . '%')
-                    ->orWhere('product_flat.meta_keywords', 'like', '%' . urldecode($term) . '%')
+                    ->where(function ($subQuery) use ($term) {
+                        $queries = explode('_', $term);
 
-
-                    // ->where(function ($subQuery) use ($term) {
-                    //     $queries = explode('_', $term);
-
-                    //     foreach (array_map('trim', $queries) as $value) {
-                    //         $subQuery->orWhere('product_flat.name', 'like', '%' . urldecode($value) . '%')
-                    //             ->orWhere('product_flat.short_description', 'like', '%' . urldecode($value) . '%');
-                    //     }
-                    // })
+                        foreach (array_map('trim', $queries) as $value) {
+                            $subQuery->orWhere('product_flat.name', 'like', '%' . urldecode($value) . '%')
+                                ->orWhere('product_flat.short_description', 'like', '%' . urldecode($value) . '%');
+                        }
+                    })
                     ->orderBy('product_id', 'desc');
             })->paginate(16);
         }
