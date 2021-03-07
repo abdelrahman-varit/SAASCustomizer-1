b9720450beec030458f7c4530ac3eab8e6269abd
Md Rezaul Karim <rkreza24@gmail.com>
2021-03-07 06:02:18 +0600

    Update ProductRepository.php

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -417,9 +417,8 @@ class ProductRepository extends Repository
                         $queries = explode('_', $term);
 
                         foreach (array_map('trim', $queries) as $value) {
-                            $subQuery->where('product_flat.name', 'like', '%' . urldecode($value) . '%')
-                                ->orWhere('product_flat.short_description', 'like', '%' . urldecode($value) . '%')
-                                ->orWhere('product_flat.meta_keywords', 'like', '%' . urldecode($value) . '%');
+                            $subQuery->orWhere('product_flat.name', 'like', '%' . urldecode($value) . '%')
+                                ->orWhere('product_flat.short_description', 'like', '%' . urldecode($value) . '%');
                         }
                     })
                     ->orderBy('product_id', 'desc');
