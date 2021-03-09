71b2f4fffaaba5ba895501d19ffda338a0ad5172
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-03-09 14:43:15 +0600

    Migration company_id

---
+++ packages/Webkul/SAASCustomizer/src/Database/Migrations/2020_02_27_170538_alter_tables_company_id_buynoir.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterSuperAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::getConnection()->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+
+        // Super Channel package migration alterations
+        Schema::table('attribute_option_translations', function (Blueprint $table) {
+            $table->integer('company_id')->unsigned()->nullable();
+            //$table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('company_id')->unsigned()->nullable();
+            //$table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
