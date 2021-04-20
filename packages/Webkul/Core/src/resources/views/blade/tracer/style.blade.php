9407515840e711c12f5badebb8fa4b1aa1efccde
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-04-20 14:24:45 +0600

    style tag remove update

--- packages/Webkul/Core/src/resources/views/blade/tracer/style.blade.php
+++ packages/Webkul/Core/src/resources/views/blade/tracer/style.blade.php
@@ -1,40 +1,5 @@
-<style>
-    .path-hint {
-        border: solid 1px transparent;
-        padding: 1px;
-    }
 
-    .path-hint:hover {
-        border: 1px solid red;
-    }
-
-    .path-hint-tooltip {
-        padding: 0px 10px;
-        position: absolute;
-        background: #000000;
-        z-index: 10000;
-        color: #fff;
-        font-size: 10px;
-    }
-
-    .path-hint-tooltip h4 {
-        margin-top: 5px;
-        margin-bottom: 3px;
-        color: #fff;
-        font-size: 12px;
-    }
-
-    .path-hint-tooltip ul li {
-        margin-bottom: 3px;
-    }
-
-    .main-container-wrapper .product-card .product-image img {
-        height: auto;
-        max-width: 100%;
-    }
-</style>
-
-<script>
+<script type="text/javascript">
     window.addEventListener("load", function(event) {
         $('.testing').each(function(index) {
             if ($(this).siblings(':not(.path-hint)').length == 1
