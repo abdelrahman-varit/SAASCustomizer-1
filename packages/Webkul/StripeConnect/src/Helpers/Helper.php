9cbbba7abf3c097e69dd7e142ef14fb79d97ed64
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-04-21 15:53:58 +0600

    Stripe connect integration

--- packages/Webkul/StripeConnect/src/Helpers/Helper.php
+++ packages/Webkul/StripeConnect/src/Helpers/Helper.php
@@ -135,46 +135,7 @@ class Helper {
         $cart->cart_currency_code = 'usd';
         $description = json_encode($sellerUser);
 
-        if ( core()->getConfigData('sales.paymentmethods.stripe.fees') == 'customer' && isset($cart->payment) && $cart->payment_method == 'stripe' ) {
-            
-            // try {
-            //     $applicationFee = $cart->base_grand_total;
-            //     $applicationFee = (0.029 * $applicationFee) + (0.02 * $applicationFee) + 0.3;
-
-            //     $cart->update([
-            //         'base_grand_total'  => $cart->base_grand_total + $applicationFee,
-            //         'grand_total'       => $cart->grand_total + core()->convertPrice($applicationFee),
-            //     ]);    
-
-            //     if  ( $customerId != '' ) {
-            //         $result = PaymentIntent::create([
-            //             'amount'               => round($cart->grand_total, 2) * 100,
-            //             'customer'             => $customerId,
-            //             'currency'             => $cart->cart_currency_code,
-            //             'statement_descriptor' => $this->statementDescriptor,
-            //             "description"          => $description,
-            //             'receipt_email'        => $cart->customer_email,
-            //             'transfer_data'        => [
-            //                       'destination'    => $sellerUserId,
-            //             ],
-            //         ]);
-            //     } else {
-            //         $result = PaymentIntent::create([
-            //             'amount'                => round($cart->grand_total, 2) * 100,
-            //             'currency'              => $cart->cart_currency_code,
-            //             'payment_method_types'  => ['card'],
-            //             'statement_descriptor'  => $this->statementDescriptor,
-            //             "description"           => $description,
-            //             'receipt_email'         => $cart->customer_email,
-            //             'transfer_data'         => [
-            //                         'destination'   => $sellerUserId,
-            //                     ],
-            //         ]);
-            //     }
-            // } catch (Exception $e) {
-            //     return $e;
-            // }
-        } else {
+       
             try {
                 if  ( $customerId != '' ) {
                     $result = PaymentIntent::create([
@@ -204,7 +165,7 @@ class Helper {
             } catch (\Exception $e) {
                 return $e;
             }   
-        }
+        
         
         return $result;
     }
