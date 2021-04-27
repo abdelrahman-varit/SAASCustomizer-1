64f85511747173d614766987d57571c420d72447
Md Rezaul Karim <rkreza24@gmail.com>
2021-04-27 18:07:55 +0600

    Update StripeConnectController.php

--- packages/Webkul/StripeConnect/src/Http/Controllers/StripeConnectController.php
+++ packages/Webkul/StripeConnect/src/Http/Controllers/StripeConnectController.php
@@ -604,23 +604,23 @@ class StripeConnectController extends Controller
         **/
         //$this->orderRepository->update(['status' => 'processing'], $this->order->id);
         $order->status = 'processing';
-        $this->orderRepository->updateOrderStatusManually($order);
+        $this->orderRepository->updateOrderStatus($order);
         
         $this->invoiceRepository = app('Webkul\Sales\Repositories\InvoiceRepository');
 
-        if ($this->order->canInvoice()) {
-            $invoice = $this->invoiceRepository->create($this->prepareInvoiceData());
+        // if ($this->order->canInvoice()) {
+        //     $invoice = $this->invoiceRepository->create($this->prepareInvoiceData());
 
-            $this->invoiceRepository->update([
-                    'grand_total' => $this->order->grand_total,
-                    'base_grand_total' => $this->order->base_grand_total
-            ], $invoice->id);
+        //     $this->invoiceRepository->update([
+        //             'grand_total' => $this->order->grand_total,
+        //             'base_grand_total' => $this->order->base_grand_total
+        //     ], $invoice->id);
 
-            $this->orderRepository->update([
-                    'grand_total_invoiced' => $this->order->grand_total,
-                    'base_grand_total_invoiced' => $this->order->base_grand_total
-            ], $this->order->id);
-        }
+        //     $this->orderRepository->update([
+        //             'grand_total_invoiced' => $this->order->grand_total,
+        //             'base_grand_total_invoiced' => $this->order->base_grand_total
+        //     ], $this->order->id);
+        // }
 
         Cart::deActivateCart();
 
