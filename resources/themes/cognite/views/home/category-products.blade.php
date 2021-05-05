c5ee7cd1fe057e0744519d53105d0c37316b6ad7
Md Rezaul Karim <rkreza24@gmail.com>
2021-05-05 15:43:33 +0600

    Update category-products.blade.php

--- resources/themes/cognite/views/home/category-products.blade.php
+++ resources/themes/cognite/views/home/category-products.blade.php
@@ -1,15 +1,20 @@
-@if (app('Webkul\Product\Repositories\ProductRepository')->getAll(10)->count())
+@if (app('Webkul\Product\Repositories\ProductRepository')->getAll()->count())
+
+@php
+    $slug = $category;
+    $category = app('Webkul\Category\Repositories\CategoryRepository')->findBySlugOrFail($slug);
+    $products = app('Webkul\Product\Repositories\ProductRepository')->getAll($category->id);
+@endphp
 <div class="main-container-wrapper" style="margin-top: 10vh;">
     <section class="featured-products">
 
         <div class="featured-heading">
-            <div class="col-3"></div>
+            <div class="col-3">{{$category->name}}</div>
             <div class="col-9"><hr></div>
-            {{-- <div class="col-2"><a class="btn-dark" href="/">More -></a></div> --}}
         </div>
 
         <div class="product-grid-4">
-            @foreach (app('Webkul\Product\Repositories\ProductRepository')->getAll(10) as $productFlat)
+            @foreach ($products as $productFlat)
                 @include ('shop::products.list.card', ['product' => $productFlat])
 
             @endforeach
