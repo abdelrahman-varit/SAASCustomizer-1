5b1af54e602d78301b3777211922dcb5d018861d
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-05-06 16:07:52 +0600

    Home default Image

--- resources/themes/cognite/views/home/advertisements/advertisement-two.blade.php
+++ resources/themes/cognite/views/home/advertisements/advertisement-two.blade.php
@@ -24,6 +24,11 @@
                     <a @if (isset($one)) href="{{ $one }}" @endif >
                         <img class="col-12" src="{{ asset('/storage/' . $advertisementTwo[0]) }}" />
                     </a>
+                @else
+                    <a @if (isset($one)) href="{{ $one }}" @endif >
+                        <img class="col-12" src="{{ asset('/themes/congnite/assets/images/banner/advertise-3-1.jpg') }}" />
+                    </a>
+
                 @endif
                 </div>
 
@@ -32,6 +37,11 @@
                         <a @if (isset($two)) href="{{ $two }}" @endif>
                             <img class="col-12 offers-ct-top" src="{{ asset('/storage/' . $advertisementTwo[1]) }}" />
                         </a>
+                        @else
+                    <a @if (isset($one)) href="{{ $one }}" @endif >
+                        <img class="col-12" src="{{ asset('/themes/congnite/assets/images/banner/advertise-3-2.jpg') }}" />
+                    </a>
+
                     @endif
                 </div>
 
@@ -41,6 +51,11 @@
                         <a @if (isset($three)) href="{{ $three }}" @endif >
                             <img class="col-12 offers-ct-bottom" src="{{ asset('/storage/' . $advertisementTwo[2]) }}" />
                         </a>
+                        @else
+                    <a @if (isset($one)) href="{{ $one }}" @endif >
+                        <img class="col-12" src="{{ asset('/themes/congnite/assets/images/banner/advertise-3-3.jpg') }}" />
+                    </a>
+
                     @endif
                 </div>
             </div>
@@ -53,18 +68,18 @@
         <div class="row">
             <div class="col-4">
                 <a @if (isset($one)) href="{{ $one }}" @endif style="flex: 1;">
-                    <img class="col-12" src="{{ asset('/themes/velocity/assets/images/big-sale-banner.png') }}" />
+                    <img class="col-12" src="{{ asset('/themes/congnite/assets/images/banner/advertise-3-1.jpg') }}" />
                 </a>
             </div>
             <div class="col-4">
                 <a @if (isset($two)) href="{{ $two }}" @endif >
-                    <img class="col-12 offers-ct-top" src="{{ asset('/themes/velocity/assets/images/seasons.png') }}" />
+                <img class="col-12" src="{{ asset('/themes/congnite/assets/images/banner/advertise-3-2.jpg') }}" />
                 </a>
             </div>
 
             <div class="col-4" >
                 <a @if (isset($three)) href="{{ $three }}" @endif >
-                    <img class="col-12 offers-ct-bottom" src="{{ asset('/themes/velocity/assets/images/deals.png') }}" />
+                <img class="col-12" src="{{ asset('/themes/congnite/assets/images/banner/advertise-3-3.jpg') }}" />
                 </a>
             </div>
         </div>
