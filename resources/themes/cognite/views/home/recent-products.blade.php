4f36aa908af290705952d40c12b3214e1b0bb672
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-05-06 06:45:10 +0600

    homepage implementation

--- resources/themes/cognite/views/home/recent-products.blade.php
+++ resources/themes/cognite/views/home/recent-products.blade.php
@@ -1,10 +1,10 @@
 @if (app('Webkul\Product\Repositories\ProductRepository')->getNewProducts()->count())
 <div class="main-container-wrapper" style="margin-top: 0px;">
-    <section class="featured-products" style="padding:0px 15px;margin-top:-40px">
+    <section class="featured-products" style="padding:0px 0px;margin-top:-40px;overflow:hidden">
 
         <div class="featured-heading">
-            <div class="col-4">{{ __('velocity::app.products.recently-viewed') }}</div>
-            <div class="col-8" style="flex:0 0 75%"><hr></div>
+            <div class="col-5">{{ __('velocity::app.products.recently-viewed') }}</div>
+            <div class="col-7" style="flex:0 0 75%"><hr></div>
         </div>
 
         <div class="product-grid-4">
