230a4a092850195f23da7bed573a53447ff5cbfd
Md Rezaul Karim <rkreza24@gmail.com>
2021-05-20 01:48:18 +0600

    Update DataPurger.php

--- packages/Webkul/SAASCustomizer/src/Helpers/DataPurger.php
+++ packages/Webkul/SAASCustomizer/src/Helpers/DataPurger.php
@@ -1056,7 +1056,7 @@ class DataPurger
                 }
             }
         }
-        dd($data);
+        dd($data1);
 
         $product1_update = $this->productRepository->update($data1, $productAttributes->id);
         
