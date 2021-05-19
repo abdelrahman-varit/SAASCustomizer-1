962bba78c166bb5634d80ed7e6f559f0f618d2ef
Md Rezaul Karim <rkreza24@gmail.com>
2021-05-19 23:22:06 +0600

    Update DataPurger.php

--- packages/Webkul/SAASCustomizer/src/Helpers/DataPurger.php
+++ packages/Webkul/SAASCustomizer/src/Helpers/DataPurger.php
@@ -1011,7 +1011,6 @@ class DataPurger
                 "status"                => 1,
                 "color"                 => 10,
                 "size"                  => 15,
-                "guest_checkout"        => 1,
                 "short_description"     => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
                     ",
                 "description"           => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries    
