c5847bade57fe463d11bbf4b8d53c57971c62c95
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-05-26 20:58:30 +0600

    email template logo url fixing

--- resources/themes/cognite/views/emails/layouts/logo.blade.php
+++ resources/themes/cognite/views/emails/layouts/logo.blade.php
@@ -1,5 +1,5 @@
 @if ($logo = core()->getCurrentChannel()->logo_url)
     <img src="{{ $logo }}" alt="{{ config('app.name') }}" style="height: 40px; width: 110px;"/>
 @else
-<img src="{{ asset('themes/'.core()->getCurrentChannel()->theme.'/assets/images/logo.svg') }}">
+<img src="{{ asset('themes/'.core()->getCurrentChannel()->theme.'/assets/images/logo.svg') }}" style="width:120px;height:auto">
 @endif 
