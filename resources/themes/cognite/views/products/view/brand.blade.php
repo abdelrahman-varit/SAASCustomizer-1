e950555f8b4fabd7a37a8594c1fe95f27b42d9ef
Ebadul Hossain <ebadul@rootnext.com>
2021-06-15 23:25:01 +0600

    Update brand.blade.php

--- resources/themes/cognite/views/products/view/brand.blade.php
+++ resources/themes/cognite/views/products/view/brand.blade.php
@@ -9,22 +9,22 @@
                 @foreach ($customAttributeValues as $attribute)
                     
                         @if ($attribute['label']=="Brand")
-                            <div  class="other-info-title" style="padding-bottom:15px">{{ $attribute['admin_name'] }} :</div>
+                            <div  class="other-info-title brand" style="padding-bottom:15px">{{ $attribute['admin_name'] }} :</div>
                         @endif
                         @if ($attribute['type'] == 'file' && $attribute['value'])
-                            <div class="other-info-value" >
+                            <div class="other-info-value file" >
                                 <a  href="{{ route('shop.product.file.download', [$product->product_id, $attribute['id']])}}">
                                     <i class="icon sort-down-icon download"></i>
                                 </a>
                             </div>
                         @elseif ($attribute['type'] == 'image' && $attribute['value'])
-                            <div class="other-info-value" >
+                            <div class="other-info-value image" >
                                 <a href="{{ route('shop.product.file.download', [$product->product_id, $attribute['id']])}}">
                                     <img src="{{ Storage::url($attribute['value']) }}" style="height: 20px; width: 20px;"/>
                                 </a>
                             </div>
                         @else
-                            <div class="other-info-value" >{{ $attribute['value'] }}</div>
+                            <div class="other-info-value others" >{{ $attribute['value'] }}</div>
                         @endif
                   
                 @endforeach
