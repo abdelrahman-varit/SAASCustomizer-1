593245312212710d049a885e4a1301d4eac39219
Mohammad Wahid <m.wahid@rootnext.com>
2021-07-28 18:05:58 +0600

    Lemon Color Scheme Added

--- resources/themes/cognite/views/layouts/master.blade.php
+++ resources/themes/cognite/views/layouts/master.blade.php
@@ -28,6 +28,9 @@
     <link rel="stylesheet" href="{{ asset('themes/cognite/assets/css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('themes/cognite/assets/css/responsive.css') }}">
 
+    <!-- Color Scheme -->
+    <link rel="stylesheet" href="{{ asset('themes/cognite/assets/css/colors/lemon.css') }}">
+
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"
     integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
     crossorigin="anonymous"></script>
