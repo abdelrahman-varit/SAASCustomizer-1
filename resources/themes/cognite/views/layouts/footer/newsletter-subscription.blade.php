a44a37a88f855c0c64f06e15fcd6f55380ab9a7a
Mohammad Wahid <m.wahid@rootnext.com>
2021-08-05 16:25:10 +0600

    Update newsletter-subscription.blade.php

--- resources/themes/cognite/views/layouts/footer/newsletter-subscription.blade.php
+++ resources/themes/cognite/views/layouts/footer/newsletter-subscription.blade.php
@@ -4,16 +4,16 @@
     || core()->getConfigData('customer.settings.newsletter.subscription')
     )
     
-    <div class="newsletter-subscription">
-        <div class="main-container-wrapper">
-            @if (core()->getConfigData('customer.settings.newsletter.subscription'))
+    @if (core()->getConfigData('customer.settings.newsletter.subscription'))
+        <div class="newsletter-subscription">
+            <div class="main-container-wrapper">
                 <form action="{{ route('shop.subscribe') }}">
                     <div class="control-group">
                         <input type="email" name="subscriber_email" class="control" placeholder="{{ __('velocity::app.customer.login-form.your-email-address') }}" required>
                         <button type="submit">{{ __('shop::app.subscription.subscribe') }}</button>
                     </div>
                 </form>
-            @endif
+            </div>
         </div>
-    </div>
+    @endif
 @endif
