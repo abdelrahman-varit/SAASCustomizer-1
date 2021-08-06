e26a59c9c51900836c6ad535157c0d8052b4d479
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-08-06 17:54:06 +0600

    left menu config
    left menu config

--- resources/admin-themes/buynoir-admin/views/layouts/nav-left.blade.php
+++ resources/admin-themes/buynoir-admin/views/layouts/nav-left.blade.php
@@ -10,21 +10,21 @@
                  
                     <ul class="buynoir-menubar-sub">
                         @foreach ($menuItem['children'] as $key => $menuItemChild)
+                                
                             @if($key=="general" || $key=="content" || $key=="design")
                                 <li class="{{in_array(request()->route('slug2'),['general','content','design']) && ($key!='emails')?'active':''}}"><a href="{{ route('admin.configuration.index', 'general/'. $key) }}">{{ $key }}</a></li>
                             @elseif($key=="catalog")
                                 <li class="{{in_array(request()->route('slug2'),['inventory','products','rich_snippets'])?'active':''}}"><a href="{{ route('admin.configuration.index', 'catalog/inventory') }}">{{ $key }}</a></li>
-                            @elseif($key=="customer")                                
-                                <li class="{{in_array(request()->route('slug2'),['settings','social_login'])?'active':''}}"><a href="{{ route('admin.configuration.index', 'customer/settings') }}">{{ $key }}</a></li>
+                            @elseif($key=="dropship")
+                                <li class="{{($key=='dropship' && request()->route('slug')=="dropship" && request()->route('slug2')=="settings")?'active':''}}"><a href="{{ route('admin.configuration.index', 'dropship/settings') }}">{{ $key }} </a></li>
+                            @elseif($key=="customer" )                                
+                                <li class="{{($key=='customer' && request()->route('slug')=="customer" && request()->route('slug2')=='settings')?'active':''}}"><a href="{{ route('admin.configuration.index', 'customer/settings') }}">{{  $key }}</a></li>
                             @elseif($key=="sales")
                                 <li class="{{in_array(request()->route('slug2'),['shipping','carriers','paymentmethods','orderSettings'])?'active':''}}"><a href="{{ route('admin.configuration.index', 'sales/shipping') }}">{{ $key }}</a></li>
                             @elseif($key=="emails")
                                 <li class="{{$key=='emails' && request()->route('slug1')=="emails"?'active':''}}"><a href="{{ route('admin.configuration.index', 'emails/general') }}">{{ $key }}</a></li>
-                            @elseif($key=="dropship")
-                            <li class="{{$key=='emails' && request()->route('slug1')=="dropship"?'active':''}}"><a href="{{ route('admin.configuration.index', 'dropship/settings') }}">{{ $key }}</a></li>
-
                             @else
-                                <li class="{{ $menu->getActive($menuItemChild) }}"><a href="{{ $menuItemChild['url'] }}">{{ $key }}</a></li>
+                                <li class="{{ $menu->getActive($menuItemChild) }}"><a href="{{ $menuItemChild['url'] }}">{{ $key}}</a></li>
                             @endif
                         @endforeach
                     </ul>
