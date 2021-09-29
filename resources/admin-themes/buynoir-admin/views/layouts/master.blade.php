1c6f9a74f9992e21be1473ff7ee80fa673b431c4
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-09-29 18:41:41 +0600

    pendo
    pendo

--- resources/admin-themes/buynoir-admin/views/layouts/master.blade.php
+++ resources/admin-themes/buynoir-admin/views/layouts/master.blade.php
@@ -41,6 +41,45 @@
 
         {!! view_render_event('bagisto.admin.layout.head') !!}
 
+
+        <script>
+    (function(apiKey){
+        (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+        v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+            o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+            y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+            z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+    })('c30eb5ea-c10a-4003-6523-301d180cb010');
+
+    // in your authentication promise handler or callback
+    pendo.initialize({
+        visitor: {
+            id:              "{{time()}}"   // Required if user is logged in
+            // email:        // Recommended if using Pendo Feedback, or NPS Email
+            // full_name:    // Recommended if using Pendo Feedback
+            // role:         // Optional
+
+            // You can add any additional visitor level key-values here,
+            // as long as it's not one of the above reserved names.
+        },
+
+        account: {
+            id:           "{{time()}}" // Required if using Pendo Feedback
+            // name:         // Optional
+            // is_paying:    // Recommended if using Pendo Feedback
+            // monthly_value:// Recommended if using Pendo Feedback
+            // planLevel:    // Optional
+            // planPrice:    // Optional
+            // creationDate: // Optional
+
+            // You can add any additional account level key-values here,
+            // as long as it's not one of the above reserved names.
+        }
+    });
+
+    </script>
+
+    
     </head>
 
     <body @if (core()->getCurrentLocale() && core()->getCurrentLocale()->direction == 'rtl') class="rtl" @endif style="scroll-behavior: smooth;">
