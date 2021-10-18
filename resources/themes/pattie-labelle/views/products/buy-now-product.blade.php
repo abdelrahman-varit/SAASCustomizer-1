f2f7acbf8356c1cc80505f2a3b1a90a693f46003
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2021-10-18 14:03:07 +0600

    Booking type product -> booking now button fixing
    Booking type product -> booking now button fixing

--- resources/themes/pattie-labelle/views/products/buy-now-product.blade.php
+++ resources/themes/pattie-labelle/views/products/buy-now-product.blade.php
@@ -1,7 +1,7 @@
 {!! view_render_event('bagisto.shop.products.buy_now.before', ['product' => $product]) !!}
-
+@if(!$product->type=="booking")
 <button type="submit" class="btn btn-black buynow" {{ ! $product->isSaleable(1) ? 'disabled' : '' }}>
     {{ __('shop::app.products.buy-now') }}
 </button>
-
+@endif
 {!! view_render_event('bagisto.shop.products.buy_now.after', ['product' => $product]) !!}
